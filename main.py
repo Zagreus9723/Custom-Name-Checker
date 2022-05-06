2fefdd9f651162784dd693ccedbb8a32a774c17c
Apophis <61201959+Apophis52@users.noreply.github.com>
2022-05-06 10:44:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if os.path.exists(file):
             for x in codes:
                 driver.get(f"{x}")
                 e = driver.title
-                if f"{response} in f"{driver.title.lower()}":
+                if f"{response} in f"{e}":
                     r = open('valid.txt', 'a')
                     r.write("------------------------\n")
                     r.write(f"{x}\n")
@@ -32,7 +32,7 @@ if os.path.exists(file):
             for x in codes:
                 driver.get(f"{site}{x}")
                 e = driver.title
-                if f"{response} in f"{driver.title.lower()}":
+                if f"{response} in f"{e}":
                     r = open('valid.txt', 'a')
                     r.write("------------------------\n")
                     r.write(f"{site}{x}\n")
